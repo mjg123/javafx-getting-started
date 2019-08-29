5c6460eb2abe60d26f019a401ac631ea5768c30a
Matthew Gilliard <matthew@gilliard.lol>
2019-08-29 12:06:21 +0100

    javafx-hello-world

--- src/main/java/com/example/javafx/Hello.java
+++ src/main/java/com/example/javafx/Hello.java
@@ -1,4 +1,27 @@
 package com.example.javafx;
 
-public class Hello {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Hello extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        Label label = new Label("Hello World from JavaFX!");
+        label.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
 }
